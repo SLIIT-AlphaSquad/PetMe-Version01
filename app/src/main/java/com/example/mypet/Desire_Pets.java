dde8d30566bdc0deedf5331bb1f0f023a9fbcfb1
NushNiz <it20207618@my.sliit.lk>
2021-08-13 02:26:11 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mypet/Desire_Pets.java
@@ -0,0 +1,14 @@
+package com.example.mypet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Desire_Pets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desire_pets);
+    }
+}
---
+++ app/src/main/java/com/example/mypet/Sell_Donate.java
@@ -0,0 +1,14 @@
+package com.example.mypet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sell_Donate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_donate);
+    }
+}
